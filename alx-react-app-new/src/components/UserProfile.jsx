950e128d0b656a6b3c58ad50ce9b484ac537606f
Onyeke Adams Mike <onyekegodstime@gmail.com>
2024-09-21 09:17:02 +0100

    styled the duplicated file

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserProfile(props) {
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
+      <p>Age: <span style={{fontSize:'20px',fontweight:'bold'}}>{props.age}</span></p>
+      <p>Bio: <span style={{fontSize:'20px',fontweight:'bold'}}>{props.bio}</span></p>
+    </div>
+  )
+}
+
+export default UserProfile
