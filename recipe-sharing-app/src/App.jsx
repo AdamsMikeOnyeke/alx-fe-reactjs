4bde037940c139dedfa44b3442a240828b2494cc
Onyeke Adams Mike <onyekegodstime@gmail.com>
2024-09-26 23:29:12 +0100

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,18 +5,20 @@ import './App.css'
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route path="/" element={<div><AddRecipeForm /><RecipeList /></div>} />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<div><AddRecipeForm /><RecipeList /></div>} />
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
