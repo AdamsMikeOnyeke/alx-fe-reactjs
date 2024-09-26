ea5b0979f620eb073286aceaf0e6fddf3cdc1aaf
Onyeke Adams Mike <onyekegodstime@gmail.com>
2024-09-26 23:22:04 +0100

    Enhance recipe sharing app with recipe management features

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,16 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-    <h1>Recipe Sharing App</h1>
-    <AddRecipeForm />
-    <RecipeList />
-  </div>
+      <h1>Recipe Sharing App</h1>
+      <Routes>
+        <Route path="/" element={<div><AddRecipeForm /><RecipeList /></div>} />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </div>
   )
 }
+
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={Number(id)} />;
+};
 
 export default App
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,7 +6,13 @@ const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) => set((state) => ({
     recipes: [...state.recipes, newRecipe]
   })),
-  setRecipes: (recipes) => set({ recipes })
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+  })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
-
-export { useRecipeStore };
